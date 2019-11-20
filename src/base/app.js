856e5ea12f38123bb6e1fbf26c4968cc6b7faaac
e-park <58010503+e-park@users.noreply.github.com>
2019-11-20 14:34:32 -0800

    Add files via upload

---
+++ src/base/app.js
@@ -0,0 +1,17 @@
+
+
+export default class App {
+  constructor (el, template) {
+    this.el = el;
+    this.template = template;
+  }
+
+  render () {
+    this.el.innerHTML = this.template;
+    this._afterRender();
+  }
+
+  _afterRender () {
+    // intentionally left blank
+  }
+}
---
+++ src/settings.js
@@ -0,0 +1,8 @@
+
+
+const settings = {
+  'app-id': '42f5e054',
+  'app-key': 'f1e2476dd5e85746f8a8bb89c70cb58e'
+};
+
+export default settings;
